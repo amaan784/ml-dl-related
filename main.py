90a14a4ffc64e3019dd0606734dbb937bf0ff776
Amaan Sheikh <100550299+amaan784@users.noreply.github.com>
2024-04-26 23:58:28 -0400

    tried negative sentiment sentence

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 # load huggin face transofrmers library
 from transformers import pipeline 
 
-def sentiment_analysis_1():
-  # create a pipleine object and input a task
+def sentiment_analysis():
+    # create a pipleine object and input a task
+    # works in 3 steps -
+    # 1) pre-pocessing the texts -  applies tokenizer to the model
+    # 2) feeds the pre processed text to the model
+    # 3) post processing - shows results
     classifier = pipeline("sentiment-analysis")
+    
 
     # apply the classifier with the data we want to test
     result = classifier("I am happy to do this")
 
     print(result) # will output something like [{'label': 'POSITIVE', 'score': 0.9998675584793091}]
-  
+
+    # trying something negative
+    result = classifier("This is really horrible.")
+    print(result) # will output something like - [{'label': 'NEGATIVE', 'score': 0.9997618794441223}]
   
 if __name__ == '__main__':
-  sentiment_analysis_1()
+  sentiment_analysis()
+#   text_generation()
